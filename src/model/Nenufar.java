96421ae970dfd967f7bab33287157af91a14f53a
Adroan <31225010+Adroan@users.noreply.github.com>
2019-05-01 19:33:52 -0300

    create model class

---
+++ src/model/Nenufar.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import javax.swing.Icon;
+
+/**
+ *
+ * @author Adroan
+ */
+public abstract class Nenufar extends Peca{
+    
+    public Nenufar(Icon imagem) {
+        super(imagem);
+    }
+    
+    
+    
+}
