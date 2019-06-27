2d6f49ec4be13f2cd5110b0651e5bae78e91e533
ViniciusTomeVieira <vinny.vieira99@hotmail.com>
2019-06-26 22:39:56 -0300

    Square returning 1

--- src/Strategy/VerificarQuadrado.java
+++ src/Strategy/VerificarQuadrado.java
@@ -16,13 +16,15 @@ public class VerificarQuadrado implements VerificarFlores {
     @Override
     public int verificar(Peca[][] matriz, int linha, int coluna) {
         if (linha < 4 && coluna < 4) {
-            if (matriz[coluna][linha].getNome().length() == matriz[coluna+1][linha].getNome().length()
-                    && matriz[coluna][linha].getNome().length() == matriz[coluna][linha+1].getNome().length()
-                    && matriz[coluna][linha].getNome().length()==matriz[coluna+1][linha+1].getNome().length()) {
+                if ((matriz[coluna][linha].getNome().length() == matriz[coluna+1][linha].getNome().length() || (matriz[coluna][linha].getNome().length() + 1) == matriz[coluna+1][linha].getNome().length() || (matriz[coluna][linha].getNome().length()) == matriz[coluna+1][linha].getNome().length() + 1)
+                    && (matriz[coluna][linha].getNome().length() == matriz[coluna][linha+1].getNome().length() || (matriz[coluna][linha].getNome().length() + 1) == matriz[coluna][linha+1].getNome().length() || (matriz[coluna][linha].getNome().length()) == matriz[coluna][linha+1].getNome().length() + 1)
+                    && (matriz[coluna][linha].getNome().length()==matriz[coluna+1][linha+1].getNome().length() || (matriz[coluna][linha].getNome().length() + 1)== matriz[coluna+1][linha+1].getNome().length() || (matriz[coluna][linha].getNome().length())== matriz[coluna+1][linha+1].getNome().length() + 1)) {
                 System.out.println("retornou 1");        
                 return 1;
                     }
-        }
+            }
+            
+        
         System.out.println("retornou 0");
         return 0;
     }
